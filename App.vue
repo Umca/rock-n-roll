f6cf1545662ebd9cc4990fbfa3fc520c0d0446f1
Olya <olga.akinshyna@gmail.com>
2018-05-09 11:18:18 +0300

    save to localStorage. add song name

--- App.vue
+++ App.vue
@@ -1,9 +1,14 @@
 <template>
     <div id='app-wrapper'>
         <div id='app-control'>
-            <button v-bind:class='{active: mode === "new"}'>Start from scratch</button>
+            <button v-bind:class='{active: mode === "new"}' v-bind:disabled='mode === "new"'>Start from scratch</button>
             <button v-bind:class='{active: mode === "edit"}'>Open to edit</button>
-            <button v-bind:class='{active: mode === "save"}'>Save</button>
+            <button v-bind:class='{active: mode === "save"}' @click='save'>Save</button>
+        </div>
+        <div id='app-song'>
+            <input type=text placeholder='Name of the song'
+                :value='songName'
+                @keyup='addSongName'/>
         </div>
         <div id='app-lines'>
             <edit-line v-for='line in lines' :key='line.id' :id='line.id'></edit-line>
@@ -23,7 +28,8 @@ export default {
     data(){
         return {
             lines: [{ id: Date.now()+Math.random() * 1000 }],
-            mode: 'new'
+            mode: 'new',
+            songName: ''
         }
     },
 
@@ -34,11 +40,30 @@ export default {
     },
 
     methods: {
+        addSongName(e){
+            this.songName = e.target.value
+        },
         addLine(){
             this.lines.push({ id: Date.now()+Math.random() * 1000 })
         },
         deleteLine(val){
             this.lines = this.lines.filter(line => line.id !== val.id)
+        },
+        save(){
+            let result = {}
+            result.songName = this.songName
+            result.lines = []
+            this.$children.forEach(ch => {
+                result.lines.push({
+                    id: ch.id,
+                    lineOfSong: ch.lineOfSong,
+                    accords: ch.accords
+                })
+            })
+
+            // to local storage
+            localStorage.setItem(result.songName, JSON.stringify(result))
+
         }
     }
 }
@@ -56,6 +81,14 @@ export default {
         justify-content: space-between;
     }
 
+    #app-song{
+        margin-bottom: 20px;
+    }
+
+    #app-song input{
+        width: 100%;
+    }
+
     #app-line-control{
         display: flex;
         justify-content: flex-end;
@@ -66,7 +99,7 @@ export default {
     }
 
     .active{
-        background:indianred
+        background:indianred;
     }
 
 </style>
