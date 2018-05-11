dbad734699e1e3ca8998b3ba6aa189e8891b224e
Olya <olga.akinshyna@gmail.com>
2018-05-11 17:01:43 +0300

    tooltip. screen parts replaced by inputs

--- components/Tooltip.vue
+++ components/Tooltip.vue
@@ -1,5 +1,16 @@
 <template>
     <div  v-bind:class="{hidden: !isShown, tooltip: isShown}" >
+        <div class ='tooltip-screen-wrapper'>
+            <div class = 'tooltip-screen'>
+                <input class='chosenItem' :value='chosenTone' type='text'
+                    @keyup="chooseItem('Tone', $event)"
+                />
+                <input type='text' class='chosenItem' :value='chosenType'
+                    @keyup="chooseItem('Type', $event)"
+                />
+            </div>
+            <div class='add btn green' @click.self='add'>+</div>
+        </div>
         <div class='options-wrapper'>
             <div id='tones' class='tooltip-box'>
                 <div v-for='tone in tones' :key='tone' @click='chooseTone(tone)'>{{ tone }} </div>
@@ -8,14 +19,7 @@
                 <div v-for='type in types' :key='type' @click='chooseType(type)'>{{ type }} </div>
             </div>
         </div>
-        <div class ='tooltip-screen-wrapper'>
-            <div class = 'tooltip-screen'>
-                <div class='chosenItem'>{{chosenTone}}</div>
-                <div class='chosenItem'>{{chosenType}}</div>
-            </div>
-        </div>
         <div class='close' @click.self='close'>x</div>
-        <div class='add' @click.self='add'>+</div>
     </div>
 </template>
 <script>
@@ -40,8 +44,11 @@ export default {
         chooseTone(id){
             this.chosenTone = id
         },
+        chooseItem(mode, e){
+            this[`chosen${mode}`] = e.target.value
+        },
         close(){
-            EventBus.$emit('close', {tone: '', type: ''})
+            this.$parent.$emit('close', {tone: '', type: ''})
             this.clean()
         },
 
@@ -61,8 +68,7 @@ export default {
     .tooltip {
         position: absolute;
         width: 29vw;
-        height:14vh;
-        background-color: lightblue;
+        background-color: rgba(201, 232, 255, 0.9);
         color:black;
         text-align: center;
         border-radius: 0.5vw;
@@ -71,9 +77,11 @@ export default {
         opacity: 1;
         font-size: 1vw;
         display: flex;
-        top: -86px;
+        top: -79px;
+        left: 209px;
         min-height: 80px;
-        min-width: 300px;;
+        min-width: 300px;
+        box-sizing: border-box;
     }
 
     .tooltip::after {
@@ -84,7 +92,7 @@ export default {
         margin-left: -5px;
         border-width: 0.95vw;
         border-style: solid;
-        border-color:lightblue transparent transparent transparent;
+        border-color:rgba(201, 232, 255, 0.9) transparent transparent transparent;
     }
 
     .tooltip-box{
@@ -99,9 +107,12 @@ export default {
 
     .tooltip-box div {
         display: inline-block;
-        border: 1px solid purple;
+        border-radius: 50%;
         padding: 0.2vw 0.5vw;
         margin-left: 0.5vw;
+        font-size: 10px;
+        font-weight: 400;
+        font-family: 'Kalam', cursive;
     }
 
     .tooltip-box div:hover{
@@ -122,42 +133,64 @@ export default {
         flex-direction: column;
     }
     .tooltip-screen{
-        background: rgb(171, 233, 113);
+        background: white;
         height: 40%;
         display: flex;
+        border-radius: 5px;
     }
 
     .chosenItem{
-        border: 1px solid rebeccapurple;
         box-sizing: border-box;
         flex-grow: 1;
-        padding-top: 0.65vw;
+        font-family: 'Kalam', cursive;
+        font-size: 10px;
+        width: 100%;
+        border: transparent;
+    }
+    .chosenItem:first-child{
+        border-radius: 3px 0 0 3px;
+    }
+
+    .chosenItem:last-child{
+        border-radius:  0 3px 3px 0 ;
     }
 
     .close{
-        color:red;
+        background: white;
+        color: black;
+        font-family: 'Kalam', cursive;
+        border: 1px solid rgba(201, 232, 255, 0.9);
+        line-height: 20px;
         position: absolute;
-        left: 96%;
-        top: -6%;
-        font-family: Arial, Helvetica, sans-serif;
-        font-weight: 900;
-        cursor: pointer;
-        font-size: 1.5vw;
-    }
-
-    .add{
-        color:green;
-        position: absolute;
-        left: 96%;
-        top: 70%;
-        font-family: Arial, Helvetica, sans-serif;
+        right: -12px;
+        text-align: center;
+        top: -10px;
+        width: 24px;
         font-weight: bold;
+        -webkit-border-radius: 12px;
+        -moz-border-radius: 12px;
+        border-radius: 50%;
+        width: 20px;
+        height: 20px;
         cursor: pointer;
-        font-size: 2vw;
+        transition: 0.3s;
+    }
+    
+    .close:hover {
+        background-color: rgba(201, 232, 255, 0.9);
+        color: white;
     }
 
     .hidden{
         display: none;
+    }
+
+    .add{
+        padding: 3px 7px;
+        height: 10%;
+        line-height: 7px;
+        font-size: 20px;
+        margin-top: 10%;
     }
 </style>
 
