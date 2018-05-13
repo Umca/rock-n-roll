66cfef78f2a58fdf1e2a794123f24177097d12c4
Olya <olga.akinshyna@gmail.com>
2018-05-13 18:55:24 +0300

    manifest and service worker

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-import App from './App.vue'
+import App from './src/App.vue'
 import Vue from 'vue'
+
+if ('serviceWorker' in navigator) {
+	  navigator.serviceWorker.register('service-worker.js').then(function() {
+		console.log('CLIENT: service worker registration complete.');
+	}, function() {
+		console.log('CLIENT: service worker registration failure.');
+	});
+} else {
+	console.log('CLIENT: service worker is not supported.');
+}
 
 var app = new Vue({
     el: '#app',
