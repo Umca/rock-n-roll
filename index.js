0d8ec53deac3e8fb3b9d850dbaa4d83a29c0320d
Olya <olga.akinshyna@gmail.com>
2018-06-02 15:51:18 +0300

    service worker

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ Vue.use(VueGoogleMaps, {
 	}
 });
 
-// if ('serviceWorker' in navigator) {
-// 	  navigator.serviceWorker.register('service-worker.js').then(function() {
-// 		console.log('CLIENT: service worker registration complete.');
-// 	}, function() {
-// 		console.log('CLIENT: service worker registration failure.');
-// 	});
-// } else {
-// 	console.log('CLIENT: service worker is not supported.');
-// }
+if ('serviceWorker' in navigator) {
+	  navigator.serviceWorker.register('service-worker.js').then(function() {
+		console.log('CLIENT: service worker registration complete.');
+	}, function() {
+		console.log('CLIENT: service worker registration failure.');
+	});
+} else {
+	console.log('CLIENT: service worker is not supported.');
+}
 
 var app = new Vue({
     el: '#app1',
